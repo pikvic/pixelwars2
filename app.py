1a907546a3eb615d47e5c3a471ff0260bf5a25c0
pikvic <truepikvic@gmail.com>
2021-12-22 12:16:42 +1000

    Add cooldown and cookies

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, Response
+from fastapi import responses
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import List
+import datetime
+import uuid
+
+
+def cooldown(player_id, players, timeout=datetime.timedelta(seconds=3)):
+    timestamp = players[player_id]
+    timedelta = datetime.datetime.now() - timestamp 
+    if timedelta < timeout:
+        return True, timeout - timedelta 
+    else: 
+        return False, timeout - timedelta
+
+     
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -27,6 +41,8 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+players = {}
+
 
 size = 10
 image = ["green" for i in range(size * size)]
@@ -34,8 +50,17 @@ image = ["green" for i in range(size * size)]
 print(image)
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):    
-    return templates.TemplateResponse("index.html", {"request": request, "image": image})
+async def root(request: Request):
+    player_id = request.cookies.get('player_id')
+    if not player_id:
+        player_id = str(uuid.uuid4())
+        players[player_id] = datetime.datetime.now()
+    if player_id not in players:
+        player_id = str(uuid.uuid4())
+        players[player_id] = datetime.datetime.now()
+    response = templates.TemplateResponse("index.html", {"request": request, "image": image})
+    response.set_cookie(key='player_id', value=player_id)
+    return response
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -44,9 +69,15 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             data = await websocket.receive_text()
             print(data, websocket)
-            index_str, color = data.split()
-            index = int(index_str.replace('p', '')) - 1
-            image[index] = color
-            await manager.broadcast(f"{index_str} {color}")
+            index_str, color, player_id = data.split()
+            index = int(index_str.replace('p', '')) - 1       
+            is_cooldown, timedelta = cooldown(player_id, players)
+            if is_cooldown:
+                msg = f"cooldown {timedelta.total_seconds()}"
+                await manager.send_personal_message(msg, websocket)
+            else:
+                image[index] = color
+                players[player_id] = datetime.datetime.now()
+                await manager.broadcast(f"{index_str} {color}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
