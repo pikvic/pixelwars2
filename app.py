80e04be95ec884178ac5e2e2cd038b570ff93a2c
pikvic <truepikvic@gmail.com>
2021-12-24 01:32:59 +1000

    New design, add online players, phrases

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import datetime
 import uuid
 
 
+FIELD_SIZE = 50
+CELL_SIZE = 10
+
+
 def cooldown(player_id, players, timeout=datetime.timedelta(seconds=3)):
     timestamp = players[player_id]
     timedelta = datetime.datetime.now() - timestamp 
@@ -37,6 +41,9 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+            
+    def get_online(self):
+        return len(self.active_connections)
 
 
 manager = ConnectionManager()
@@ -44,10 +51,9 @@ manager = ConnectionManager()
 players = {}
 
 
-size = 10
-image = ["green" for i in range(size * size)]
+size = FIELD_SIZE
+image = ["white" for i in range(size * size)]
 
-print(image)
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
@@ -58,17 +64,18 @@ async def root(request: Request):
     if player_id not in players:
         player_id = str(uuid.uuid4())
         players[player_id] = datetime.datetime.now()
-    response = templates.TemplateResponse("index.html", {"request": request, "image": image})
+    response = templates.TemplateResponse("index.html", {"request": request, "image": image, "field_size": FIELD_SIZE, "cell_size": CELL_SIZE, "online": manager.get_online()})
     response.set_cookie(key='player_id', value=player_id)
     return response
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
+    await manager.broadcast(f"online {manager.get_online()}")
     try:
         while True:
             data = await websocket.receive_text()
-            print(data, websocket)
+           # print(data, websocket)
             index_str, color, player_id = data.split()
             index = int(index_str.replace('p', '')) - 1       
             is_cooldown, timedelta = cooldown(player_id, players)
@@ -81,3 +88,4 @@ async def websocket_endpoint(websocket: WebSocket):
                 await manager.broadcast(f"{index_str} {color}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
+        await manager.broadcast(f"online {manager.get_online()}")
